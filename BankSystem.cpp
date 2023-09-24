407d1813ca1cc53a5f485f08bb3531fb5bccaa50
Youssef Abd-El-Ghfar Abd-El-Twab <91377316+youssefeissa18@users.noreply.github.com>
2023-09-25 02:39:18 +0300

    Create Main And Finish the Project

--- BankSystem.cpp
+++ BankSystem.cpp
@@ -83,12 +83,22 @@ public:
             cout << "Not Founded" << endl;
         }  
     }
+    BankAccount* findAccount(int accountNum){
+			for(int i = 0; i< bankAccount.size();i++){
+				if(bankAccount[i].getAccountNum() == accountNum){
+					return &bankAccount[i];
+				}
+			}
+		}
 
 };
 
 char op;
 int choice;
 int main(){
+    cout << "================================================Bank-Management-System-Project============================================" << endl;
+    cout<<"Do You Want to Countinue or Exit [Y/N] ??";
+    cin >> op;
     while (op == 'Y' || op == 'y')
     {
         cout << "1- Create New Account" << "\n2- Show All Account" << "\n3- Search Account"<< "\n4- Deposit Money" <<  "\n5- Withdraw Money"<< "\n6- Balance Inquiry"<< "\n7- Exit" << endl;
@@ -110,11 +120,84 @@ int main(){
                 BankManage Bank;
                 Bank.addAccount(name,accountNum,balance);
                 cout << "Your Account Create Successfully" << endl;
-                
             }
             break;
-        
+        case 2:
+            {
+                cout << "====================================================" << endl;
+                BankManage Bank;
+                Bank.showAllAccount();
+                cout << "====================================================" << endl;
+            }
+            break;
+        case 3:
+            {
+                cout << "====================================================" << endl;
+                BankManage Bank;
+                int accountNum;
+                cout << "Enter Account Number You Want : ";
+                cin >> accountNum;
+                cout << endl; 
+                Bank.searchAccount(accountNum);
+                cout << "====================================================" << endl;
+            }
+            break;
+        case 4:
+            {
+                cout << "====================================================" << endl;
+                double amount;
+                BankManage Bank1;
+                int accountNum;
+                cout << "Enter Account Number You Want : ";
+                cin >> accountNum;
+                cout << endl; 
+                BankAccount *Bank = Bank1.findAccount(accountNum);
+                if (Bank != NULL)
+                {
+                    cout << "Enter The Amount : ";
+                    cin >> amount;
+                    cout << endl; 
+                    Bank->deposit(amount);
+                    cout<< amount <<" Deposit Successfully ...."<<endl;
+                }
+                else
+                {
+                    cout << "Your Account not Found" << endl;
+                }
+                
+                cout << "====================================================" << endl;
+            }
+            break;
+        case 5:
+            {
+                cout << "====================================================" << endl;
+                double amount;
+                BankManage Bank1;
+                int accountNum;
+                cout << "Enter Account Number You Want : ";
+                cin >> accountNum;
+                cout << endl; 
+                BankAccount *Bank = Bank1.findAccount(accountNum);
+                if (Bank != NULL)
+                {
+                    cout << "Enter The Amount : ";
+                    cin >> amount;
+                    cout << endl; 
+                    Bank->withDraw(amount);
+                    cout<< amount <<" Withdraw Successfully ...."<<endl;
+                }
+                else
+                {
+                    cout << "Your Account not Found" << endl;
+                }
+                cout << "====================================================" << endl;
+            }
+            break;
+        case 6:
+            exit(0);
+            break;
         default:
+            cout << "Invalide Choice try Again " << endl;
             break;
         }
         cout<<"Do You Want to Countinue or Exit [Y/N] ??";
