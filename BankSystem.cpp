609584fd9e79443dc4a85fa0338a7cdea35bfe31
Youssef Abd-El-Ghfar Abd-El-Twab <91377316+youssefeissa18@users.noreply.github.com>
2023-09-25 02:18:07 +0300

    Create Class Bank Manage

--- BankSystem.cpp
+++ BankSystem.cpp
@@ -1,3 +1,11 @@
+//  1. Create New Account 
+//  2. Show All Account    
+//  3. Search Account or Find Account      
+//  4. Deposit Money       
+//  5. Withdraw Money       
+//  6. Balance Inquiry      
+//  7. Exit
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -41,27 +49,78 @@ public:
             cout << "Your Balance is not have enough money" << endl;
         }
     }
+};
 
+class BankManage{
+private:
+    vector<BankAccount> bankAccount;
+public:
+    BankManage(){}
+    void addAccount(string name, int accountNum, double balance){
+        bankAccount.push_back(BankAccount(name,accountNum,balance));
+    }
 
+    void showAllAccount(){
+        cout << "The Accounts : " << endl;
+        for (int i = 0; i < bankAccount.size(); i++)
+        {
+            cout << "\t\tName : " << bankAccount[i].getName() << "\t\tAccount Number : " << bankAccount[i].getAccountNum() << "\t\tBalance  : " << bankAccount[i].getBalance() << endl;
+        }
+    }
+    void searchAccount(int accountNum){
+        int flag = 0;
+        for (int i = 0; i < bankAccount.size(); i++)
+        {
+            if (bankAccount[i].getAccountNum() == accountNum)
+            {
+                flag = 1;
+                cout << "\t\tName : " << bankAccount[i].getName() << "\t\tAccount Number : " << bankAccount[i].getAccountNum() << "\t\tBalance  : " << bankAccount[i].getBalance() << endl;
+            }
+            
+        }
+        if (flag == 0)
+        {
+            cout << "Not Founded" << endl;
+        }  
+    }
 
 };
 
-//  1. Create New Account   BA Done
-//  2. Show All Account     BM
-//  3. Search Account       Bm
-//  4. Deposit Money        BA Done
-//  5. Withdraw Money       BA Done
-//  6. Balance Inquiry      BA Done
-//  7. Exit
+char op;
+int choice;
 int main(){
-    BankAccount B1("Youssef",12,4512);
-    cout << B1.getBalance() << endl;
-    cout << B1.getAccountNum() << endl;
-    B1.deposit(1000);
-    cout << B1.getBalance() << endl;
-    B1.withDraw(10000);
-    cout << B1.getBalance() << endl;
-    cout << "Hello World" << endl;
+    while (op == 'Y' || op == 'y')
+    {
+        cout << "1- Create New Account" << "\n2- Show All Account" << "\n3- Search Account"<< "\n4- Deposit Money" <<  "\n5- Withdraw Money"<< "\n6- Balance Inquiry"<< "\n7- Exit" << endl;
+        cout << "Your Choice is";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            {
+                string name;
+                cout << "Enter Name : " ;
+                getline(cin , name);
+                int accountNum;
+                cout << "\n Enter Account Number : ";
+                cin >> accountNum;
+                int balance;
+                cout << "\n Enter Balance : ";
+                cin >> balance;
+                BankManage Bank;
+                Bank.addAccount(name,accountNum,balance);
+                cout << "Your Account Create Successfully" << endl;
+                
+            }
+            break;
+        
+        default:
+            break;
+        }
+        cout<<"Do You Want to Countinue or Exit [Y/N] ??";
+    	cin>>op;
+    }
+    
 
     return 0;
 }
